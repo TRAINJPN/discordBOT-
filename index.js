3c0eccc39b2afa717eb363c35ca2b6c01bba3345
TRAINJPN <skyjpn168@gmail.com>
2025-08-08 23:19:27 +0900

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => {
   console.log(`ログイン成功：${client.user.tag}`);
+
+  // 👇 ステータスを設定する
+  client.user.setPresence({
+    status: 'online', // 他には "idle", "dnd", "invisible" が使える
+    activities: [
+      {
+        name: '桜咲市を監視中', // 表示したい文字
+        type: ActivityType.Watching, // "Playing", "Listening", "Watching", "Competing"
+      },
+    ],
+  });
 });
 
-client.login('MTQwMzM1MjU0ODc5ODEwNzY5OA.GfG8K5.CWbWF3RBJEXW1ndqF8O_27UCr_r0D40gBwnBrY'); // ←ここを自分のBotトークンに書き換える！
+client.login('MTQwMzM1MjU0ODc5ODEwNzY5OA.GqtKHw.U9p0SUwRvLY4Tv71MQ7_CsGfuqKmZQ-NhfpSpY');
